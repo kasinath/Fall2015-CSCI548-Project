23aca911a16d1d4c3d1787173109badbe3234c83
Lakshmi Kasinathan <kasinath@usc.edu>
2015-11-27 03:11:44 -0800

    implement drag and drop

---
+++ ui/project/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+    $('#datePicker1')
+        .datepicker({
+            format: 'mm/dd/yyyy'
+        })
+        
+
+ $('#datePicker2')
+        .datepicker({
+            format: 'mm/dd/yyyy'
+        })
+        
+
+
+
+});
+
+
+var options = {
+    cell_height: 80,
+    vertical_margin: 10
+};
+$('.grid-stack').gridstack(options);
+
+
+function cityClick(id)
+{
+	var len = 3;
+	var classprefix = "city-btn-"
+	var cities = ["Los Angeles", "New York" , "London"];
+	for(i=0;i<3;i++)
+	{
+		if(i!=id)
+			$('#'+classprefix+i).removeClass("btn-primary");
+		else
+			$('#'+classprefix+i).addClass("btn-primary");
+				
+	}
+
+
+}
+function IntClick(id)
+{
+	var len = 6;
+	var classprefix = "interest-btn-"
+	var cities = ["Los Angeles", "New York" , "London"];
+	if ($('#'+classprefix+id).hasClass("btn-primary"))
+		$('#'+classprefix+id).removeClass("btn-primary");
+	else
+		$('#'+classprefix+id).addClass("btn-primary");
+	
+}
+
+function go()
+{
+
+	var len = 3;
+	var msg = "City : " ;
+	var classprefix = "city-btn-"
+var cities = ["Los Angeles", "New York" , "London"];
+	for(i=0;i<3;i++)
+	{
+		if ($('#'+classprefix+i).hasClass("btn-primary"))
+			{
+				msg +=  cities[i];
+				break;
+			}					
+	}
+
+
+	 len = 6;
+	 msg += ",\n Interests : " ;
+	var classprefix = "interest-btn-"
+	var interests = ["Literature","Movies","Art","Theatre","Music","Cooking"];
+	for(i=0;i<len;i++)
+	{
+		if ($('#'+classprefix+i).hasClass("btn-primary"))
+			{
+				
+				msg +=  interests[i] + ",";
+
+			}					
+	}	
+msg += "\n Generating results";
+
+var r = confirm(msg);
+if (r == true) {
+    window.location="results.html"
+} else {
+    txt = "You pressed Cancel!";
+}
+
+}
